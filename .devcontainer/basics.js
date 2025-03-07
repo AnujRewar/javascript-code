16747aca0ba8feaaa118554bae0c561e11c798a8
AnujRewar <75022117+AnujRewar@users.noreply.github.com>
2025-03-07 06:30:01 +0000

    basics

--- .devcontainer/basics.js
+++ .devcontainer/basics.js
@@ -1,1 +1,5 @@
-console.log("output");
+const anuj=240103017
+var address="sainswas,jhunjhunu,Rajastahn,333701"
+var current_address="imphal, manipur";
+var education="engineering";
+console.table([anuj,address,current_address,education]);
